5a295fef02c47e74e19594aaec2237907c1e9da7
Cedric Levasseur <clevasseur@fr.imshealth.com>
2017-10-12 16:40:39 +0200

    missing comma

--- files/groovy/add_ldapuser_role.groovy
+++ files/groovy/add_ldapuser_role.groovy
@@ -5,7 +5,7 @@ parsed_args = new JsonSlurper().parseText(args)
 
 try {
     // update an existing user
-    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id parsed_args.role)
+    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id, parsed_args.role)
 } catch(UserNotFoundException ignored) {
     //nothing to do 
 }
