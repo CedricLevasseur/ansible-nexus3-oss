cb898711ab7dc5305dd547eb0e15fe0e437ac5db
Cedric Levasseur <clevasseur@fr.imshealth.com>
2017-10-12 17:43:03 +0200

    implementation change, using ldap search

--- files/groovy/add_ldapuser_role.groovy
+++ files/groovy/add_ldapuser_role.groovy
@@ -2,10 +2,17 @@ import groovy.json.JsonSlurper
 import org.sonatype.nexus.security.user.UserNotFoundException
 
 parsed_args = new JsonSlurper().parseText(args)
-try {
-    // update an existing user
-  
-    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id, [parsed_args.role] )
-} catch(UserNotFoundException ignored) {
+//try {
+   
+   //search for a user in LDAP
+   criteria = new UserSearchCriteria(userId: parsed_args.usernames, source: 'LDAP')
+   users = security.securitySystem.searchUsers(criteria)
+   //user.forEach { println it }
+   users.each { 
+      security.setUserRoles(it.userId, parsed_args.role)
+      security.securitySystem.updateUser(user)
+   }
+//    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id, [parsed_args.role] )
+//} catch(UserNotFoundException ignored) {
     //nothing to do 
-}
+//}
