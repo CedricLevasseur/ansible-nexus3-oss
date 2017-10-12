dd00f39eb02e0b64c7a5e1d2cfe414c1c2598019
Cedric Levasseur <clevasseur@fr.imshealth.com>
2017-10-12 16:49:14 +0200

    List intead of a single item

--- files/groovy/add_ldapuser_role.groovy
+++ files/groovy/add_ldapuser_role.groovy
@@ -2,10 +2,10 @@ import groovy.json.JsonSlurper
 import org.sonatype.nexus.security.user.UserNotFoundException
 
 parsed_args = new JsonSlurper().parseText(args)
-
 try {
     // update an existing user
-    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id, parsed_args.role)
+  
+    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id, [parsed_args.role] )
 } catch(UserNotFoundException ignored) {
     //nothing to do 
 }
