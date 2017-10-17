bb494ab8f49556e588b126cfe63ff774fa827756
Cedric Levasseur <clevasseur@fr.imshealth.com>
2017-10-17 16:58:58 +0200

    updated script, (still not working), to be continued

--- files/groovy/add_ldapuser_role.groovy
+++ files/groovy/add_ldapuser_role.groovy
@@ -3,17 +3,20 @@ import org.sonatype.nexus.security.user.UserNotFoundException
 import org.sonatype.nexus.security.user.UserSearchCriteria
 
 parsed_args = new JsonSlurper().parseText(args)
-//try {
+try {
    
    //search for a user in LDAP
-   criteria = new UserSearchCriteria(userId: parsed_args.usernames)//, source: 'LDAP')
+   criteria = new UserSearchCriteria(userId: parsed_args.usernames, source: "LDAP")
    users = security.securitySystem.searchUsers(criteria)
    //user.forEach { println it }
-   users.each { 
+   users.each {
+      if("clevasseur".equalsIgnoreCase(it.userId)){
+	log.into("FOUNDED !!!")
+      } 
       security.setUserRoles(it.userId, [parsed_args.role])
       security.securitySystem.updateUser(it)
    }
 //    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id, [parsed_args.role] )
-//} catch(UserNotFoundException ignored) {
-    //nothing to do 
-//}
+} catch(UserNotFoundException e) {
+    log.into("User not found ",e)
+}
