2641444d3f5d5278c533e9c4b5889532b143a66b
Cedric Levasseur <clevasseur@fr.imshealth.com>
2017-10-25 14:50:36 +0200

    Multiples tries during days for setting nx-admin group a a LDAP User

--- files/groovy/add_ldapuser_role.groovy
+++ files/groovy/add_ldapuser_role.groovy
@@ -1,22 +1,71 @@
 import groovy.json.JsonSlurper
 import org.sonatype.nexus.security.user.UserNotFoundException
 import org.sonatype.nexus.security.user.UserSearchCriteria
+import org.sonatype.nexus.security.user.User
+import org.sonatype.nexus.security.role.RoleIdentifier
+import org.sonatype.nexus.security.user.UserManager
+import org.sonatype.nexus.security.user.RoleMappingUserManager;
 
-parsed_args = new JsonSlurper().parseText(args)
+//parsed_args = new JsonSlurper().parseText(args)
 try {
+
+   def admin = security.securitySystem.getUser('admin')
+   RoleIdentifier nxAdmin = admin.getRoles()[0]	
+
    
    //search for a user in LDAP
-   criteria = new UserSearchCriteria(userId: parsed_args.usernames, source: "LDAP")
+//   criteria = new UserSearchCriteria(userId: parsed_args.usernames, source: "LDAP")
+   criteria = new UserSearchCriteria(userId: "clevasseur", source: "LDAP")
+
    users = security.securitySystem.searchUsers(criteria)
    //user.forEach { println it }
-   users.each {
-      if("clevasseur".equalsIgnoreCase(it.userId)){
-	log.info("FOUNDED !!!")
+   //return users.toString()+" "+users[0].getUserId()
+//   return users[0].class.package.name + " "+ users.size()+" "+users.toString()
+   for(User u : users){
+   //UserManager userManager = security.securitySystem.getUserManager(admin.getSource());
+   UserManager userManager = security.securitySystem.getUserManager(u.getSource());
+//   userManager.setUsersRoles(u.userId, admin.getSource(), [ nxAdmin ].toSet())
+
+/*        try {
+          RoleMappingUserManager roleMappingUserManager = (RoleMappingUserManager) userManager;
+          roleMappingUserManager.setUsersRoles(
+              u.getUserId(),
+              u.getSource(),
+              nxAdmin
+          );
+        }catch(Exception e){
+	  return e.toString()
+        }	
+*/
+    	
+    u.roles = [ nxAdmin ] // = roleIds.collect{ new RoleIdentifier(DEFAULT_SOURCE, it)}
+//   security.securitySystem.updateUser(u)
+//    return u.toString()  	
+    User userModified = security.setUserRoles('admin' /*u.userId*/, [ 'nx-admin' ]) 
+//    return userModified.getRoles()
+
+      if("clevasseur".equalsIgnoreCase(u.userId)){
+	log.info("FOUNDED !!! " +u.userId )
+
+//       return security.securitySystem.listRoles('LDAP')
       } 
-      security.setUserRoles(it.userId, [parsed_args.role])
-      security.securitySystem.updateUser(it)
+
+      security.securitySystem.setUsersRoles(u.userId, 'LDAP', [nxAdmin].toSet())
+      return u	
+      //u.addRole(nxAdmin);
+
+//      security.securitySystem.setUsersRoles(u.userId, 'LDAP', [nxAdmin].toSet())
+//      security.securitySystem.updateUser(u)
    }
 //    security.setUserRoles(parsed_args.usernames, parsed_args.ldap_id, [parsed_args.role] )
-} catch(UserNotFoundException e) {
-    log.info("User not found ",e)
+
+return users[0].getRoles()	
+//return "OK"
+
+} catch(UserNotFoundException unfe) {
+    log.info("User not found "+criteria.toString(), unfe)
+    return "User not found "+criteria.toString()+" "+unfe
 }
+
+
+return "FAIL" 
